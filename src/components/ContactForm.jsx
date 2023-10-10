17c8b48c278d2db62a6ac70e92c6abffbd8bb02c
Tomee Keringer <keringer.tamas@gmail.com>
2023-10-10 12:14:20 +0200

    urlap

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -23,55 +23,13 @@ const ContactForm = () => {
     const result = await response.json();
     if (result.success) {
       alert("Hamarosan felvesszük Önnel a kapcsolatot!");
-      router.push("/");
     }
   }
 
   return (
     <div className="container " data-aos="fade-up" T>
-      <div className="my-5 text-center">
-        <h1>
-          <strong> KAPCSOLAT </strong>
-        </h1>
-        <h3>LÉPJEN KAPCSOLATBA VELÜNK!</h3>
-      </div>
-      <div className="form-style-one" data-aos="fade-up">
-        <form onSubmit={handleSubmit}>
-          <div className="messages" />
-          <div className="row controls m-5">
-                <input  className="w-full text-black" type="text" placeholder="Név*" name="name" required="required" data-error="Name is required." />
-                <div className="help-block with-errors" />
-              </div>
-          <div className="row controls m-5">
 
-                <input  className="w-full text-black"
-                  type="email"
-                  placeholder="Email cím*"
-                  name="email"
-                  required="required"
-                  data-error="Valid email is required."
-                />
-                <div className="help-block with-errors" />
-          </div>
-          <div className="row controls m-5 text-black">
-                <textarea className="w-full  "
-                  placeholder="Üzenet*"
-                  name="message"
-                  required="required"
-                  data-error="Please,leave us a message."
-                  defaultValue={""}
-                />
-                <div className="help-block with-errors" />
-            {/* End .col-12 */}
-          </div>
-          <div className="row controls w-full  text-center center">
-     
-              <button className="btn-secondary   text-center bg-slate-500 mx-auto block btn fw-500 ">Beküldés </button>
-            {/* End .col-12 */}
-          </div>
-          {/* End .row */}
-        </form>
-      </div>
+
     </div>
   );
 };
